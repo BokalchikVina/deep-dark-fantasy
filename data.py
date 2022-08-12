15e3e8756dc093669c91e06853d5c553de4cb3d1
BokalchikVina <102982119+BokalchikVina@users.noreply.github.com>
2022-08-13 00:57:11 +0500

    а

--- data.py
+++ data.py
@@ -12,5 +12,3 @@ class BotDB:
     def user_exist(self, user_id):
         result = self.cursor.execute("SELECT 'id' FROM 'Users' WHERE 'user id' = ?", (user_id,))
         return bool(len(result.fetchall()))
-
-    def
--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import telebot
 from data import BotDB
 
 con = BotDB("database.db")
+bot = telebot.TeleBot('5226303098:AAG-Cje17TQmI9xdxV3rfia0SmJJVjzLAPg')
+
+@bot.message_handlers(commands = ["start"])
+
+def start():
